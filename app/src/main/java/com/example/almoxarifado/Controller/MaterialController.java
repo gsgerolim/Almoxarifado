5c1573e982b68c8248a1d4d1b0d81d04eb1bd09f
Romulo-Moraes <romuloperes6@gmail.com>
2022-10-19 17:34:40 -0300

    Modificações para verificação de usuario

--- app/src/main/java/com/example/almoxarifado/Controller/MaterialController.java
+++ app/src/main/java/com/example/almoxarifado/Controller/MaterialController.java
@@ -3,4 +3,5 @@ package com.example.almoxarifado.Controller;
 public class MaterialController {
 
     // oi eu sou o goku!
+    // olha link aqui https://medium.com/@devesu/how-to-logout-from-git-in-windows-e17c66fe9ca8
 }
