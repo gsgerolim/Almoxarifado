75d35b2594612bc76ac6b936a2696e428a092b09
Romulo-Moraes <romuloperes6@gmail.com>
2022-10-25 22:59:09 -0300

    zika

---
+++ app/src/main/java/com/example/almoxarifado/teladecadastro.java
@@ -0,0 +1,16 @@
+package com.example.almoxarifado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class teladecadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teladecadastro);
+    }
+
+    Button btn
+}
