b0b315cada37a106e28750170e7dcfd81b901848
ProjetoDS <romuloperes6@gmail.com>
2022-11-01 22:48:16 -0300

    Modificações Aula 10/11

--- app/src/main/java/com/example/almoxarifado/MainActivity.java
+++ app/src/main/java/com/example/almoxarifado/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.almoxarifado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btnAdd = (ImageButton) findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, activityCadastro.class);
+                star
+            }
+        });
     }
 }
--- app/src/main/java/com/example/almoxarifado/activityCadastro.java
+++ app/src/main/java/com/example/almoxarifado/activityCadastro.java
@@ -3,12 +3,19 @@ package com.example.almoxarifado;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.almoxarifado.Controller.MaterialController;
 
 public class activityCadastro extends AppCompatActivity {
+    MaterialController materialController = new MaterialController();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        Button btnCadastro = (Button) findViewById(R.id.btnCadastro);
+        btnCadastro.setOnClickListener(new View.OnClickListener());
     }
 }
