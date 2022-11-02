a96545d707d8f85db77041d7218f20589ad208d4
Emily Bastos <romuloperes6@gmail.com>
2022-11-01 22:53:57 -0300

    Criamos as funcionalidades para o botão de ADD do main configurando as funcionalidades do cadastro

--- app/src/main/java/com/example/almoxarifado/Cadastro.java
+++ app/src/main/java/com/example/almoxarifado/Cadastro.java
@@ -3,12 +3,35 @@ package com.example.almoxarifado;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Cadastro extends AppCompatActivity {
+import com.example.almoxarifado.Controller.MaterialController;
+
+public class Cadastro extends AppCompatActivity
+{
+    MaterialController materialController = new MaterialController();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        Button btnCofCad = (Button) findViewById(R.id.btnConfCad);
+        EditText edtCod = (EditText) findViewById(R.id.edtCodCad);
+        EditText edtNome = (EditText) findViewById(R.id.edtNomeCad);
+        EditText edtQtd = (EditText) findViewById(R.id.edtQuanCad);
+
+        btnCofCad.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+                materialController.adcMaterial(edtCod, edtNome, edtQtd);
+            }
+        });
     }
 }
