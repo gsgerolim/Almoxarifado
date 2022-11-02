71ca0500d8f93cc38bc5c05314190e933d779dc6
Mackert <romuloperes6@gmail.com>
2022-11-01 22:53:58 -0300

    Criamos as funcionalidades para o botão de add do main e estávamos configurando as funcionalidades do cadastro

--- app/src/main/java/com/example/almoxarifado/Cadastro.java
+++ app/src/main/java/com/example/almoxarifado/Cadastro.java
@@ -3,12 +3,40 @@ package com.example.almoxarifado;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Cadastro extends AppCompatActivity {
+import com.example.almoxarifado.Controller.MaterialController;
 
+public class Cadastro extends AppCompatActivity
+{
+    MaterialController materialController = new MaterialController();
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        //Botão
+        Button btnConfCad = (Button) findViewById(R.id.btnConfCad);
+
+        //Edits
+        EditText edtCod = (EditText) findViewById(R.id.edtCodCad);
+        EditText edtNome = (EditText) findViewById(R.id.edtNomeCad);
+        EditText edtQtd = (EditText) findViewById(R.id.edtQuantCad);
+
+        //Funcionalidade
+        btnConfCad.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+
+                materialController.adcMaterial(edtCod, edtNome, edtQtd);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/almoxarifado/MainActivity.java
+++ app/src/main/java/com/example/almoxarifado/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.almoxarifado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Botões
+        ImageButton btnAdd = (ImageButton) findViewById(R.id.btnAdd);
+
+        //Funcionalidades
+        btnAdd.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, Cadastro.class);
+                startActivity(intent);
+            }
+        });
     }
 }
