34b1e9a6f907f1855b7efe913be841bd120e0de6
Romulo-Moraes <romuloperes6@gmail.com>
2022-11-23 22:30:57 -0300

    update

--- app/src/main/java/com/example/almoxarifado/Controller/MaterialController.java
+++ app/src/main/java/com/example/almoxarifado/Controller/MaterialController.java
@@ -3,11 +3,14 @@ package com.example.almoxarifado.Controller;
 import android.widget.EditText;
 
 import com.example.almoxarifado.Model.Material;
+import com.google.android.material.snackbar.Snackbar;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Scanner;
 
-public class MaterialController {
+public class MaterialController
+{
 
     /*Erros observados
 (OK) Listar especifico: ao utilizar essa opção continua listando todos os produtos
@@ -21,13 +24,15 @@ public class MaterialController {
 (OK) Importar após inserir
  */
 
-        public static void ler(Material materiais[], String nome) {
+        public static void ler(Material materiais[], String nome)
+        {
             String linha, v[], nome2;
             int cod, aux, saldo, cont = 0;
 
             Material material;
 
-            try {
+            try
+            {
                 //lendo arquivo
                 FileReader fr = new FileReader(nome);
                 //armazendando em buffer para poder utilizar
@@ -36,17 +41,22 @@ public class MaterialController {
                 linha = bfr.readLine();
                 /*Linha está organizada em 0-codigo 1-nome 2-saldo*/
 
-                while ((linha != null) && (cont < materiais.length)) {
+                while ((linha != null) && (cont < materiais.length))
+                {
                     v = linha.split(";");
                     //procurar se o codigo existe em todo o vetor
-                    if (procuraNoVetor(materiais, Integer.parseInt(v[0]))) {
+                    if (procuraNoVetor(materiais, Integer.parseInt(v[0])))
+                    {
                         //se existir vai para a proxima linha do arquivo de importação
                         System.out.println("Produto ja cadastrado " + linha);
                         linha = bfr.readLine();
-                    } else {
+                    } else
+                    {
                         //senao existir, procura a primeira posição vazia para inserir o produto
-                        for (int i = 0; i < materiais.length; i++) {
-                            if (materiais[i].getCodigo() == -1) {
+                        for (int i = 0; i < materiais.length; i++)
+                        {
+                            if (materiais[i].getCodigo() == -1)
+                            {
                                 material
                                         = new Material(Integer.parseInt(v[0]),
                                         v[1],
@@ -63,22 +73,27 @@ public class MaterialController {
                 bfr.close();
                 fr.close();
                 System.out.println(cont + " Produtos importados!");
-            } catch (Exception e) {
+            } catch (Exception e)
+            {
                 System.err.println("Erro ao importar!");
             }
         }
 
-        public static boolean procuraNoVetor(Material[] vetorParaBuscar, int codABuscar) {
+        public static boolean procuraNoVetor(Material[] vetorParaBuscar, int codABuscar)
+        {
             boolean encontrado = false;
-            for (int i = 0; i < vetorParaBuscar.length; i++) {
-                if (vetorParaBuscar[i].getCodigo() == codABuscar) {
+            for (int i = 0; i < vetorParaBuscar.length; i++)
+            {
+                if (vetorParaBuscar[i].getCodigo() == codABuscar)
+                {
                     encontrado = true;
                 }
             }
             return encontrado;
         }
 
-        public static void mostraMenu() {
+        public static void mostraMenu()
+        {
             //criando o menu de ações
             System.out.println("O que deseja fazer?");
             System.out.println("--MENU--");
@@ -90,16 +105,19 @@ public class MaterialController {
             System.out.println("6 - Remover");
         }
 
-        private static void alimentaVetor(Material[] materiais) {
+        private static void alimentaVetor(Material[] materiais)
+        {
             for (int i = 0; i < materiais.length; i++) {
                 Material materialVazio = new Material(-1, "", 0);
                 materiais[i] = materialVazio;
             }
         }
 
-        private static int buscarPorCodigo(Material[] vetorParaBuscar, int codABuscar) {
+        private static int buscarPorCodigo(Material[] vetorParaBuscar, int codABuscar)
+        {
             for (int i = 0; i < vetorParaBuscar.length; i++) {
-                if (vetorParaBuscar[i].getCodigo() == codABuscar && codABuscar != -1) {
+                if (vetorParaBuscar[i].getCodigo() == codABuscar && codABuscar != -1)
+                {
                     //caso existe o codigo armazenado a var indice
                     //guarda o valor de i e em seguida encerramos
                     //o laço!
@@ -109,6 +127,17 @@ public class MaterialController {
             return -1;
         }
 
-    public void adcMaterial(EditText edtCod, EditText edtNome, EditText edtQtd) {
+    public void adcMaterial(EditText edtCod, EditText edtNome, EditText edtQtd)
+    {
+    try
+    {
+        materialDAO.inserir(materialCadastro);
+        Snackbar.make(telaParaMostrar, "Material" + materialCadastro.getNome() +"inserido com sucesso", 1000).show();
+    }
+    catch (Exception e)
+    {
+        e.printStackTrace();
+        Snackbar.make(telaParaMostrar, "Não foi possível cadastrar o item", 1000).show();
+    }
     }
 }
