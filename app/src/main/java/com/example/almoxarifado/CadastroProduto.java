b37e15292ff79e07b275c5a36bc76c01898e78ec
Romulo-Moraes <romuloperes6@gmail.com>
2022-11-23 22:54:51 -0300

    a

--- app/src/main/java/com/example/almoxarifado/CadastroProduto.java
+++ app/src/main/java/com/example/almoxarifado/CadastroProduto.java
@@ -2,13 +2,16 @@ package com.example.almoxarifado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.almoxarifado.Controller.MaterialController;
+import com.example.almoxarifado.Model.Material;
 
 public class CadastroProduto extends AppCompatActivity {
     MaterialController materialController = new MaterialController();
@@ -23,9 +26,19 @@ public class CadastroProduto extends AppCompatActivity {
 
         btnCadastraProduto.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                materialController.adcMaterial(edtCod, edtNome, edtQtd);
+            public void onClick(View v) {
+                Material materialCadastro = new Material();
+                materialCadastro.setCodigo(Integer.parseInt(edtCod.getText().toString()));
+                materialCadastro.setNome(edtNome.getText().toString());
+                materialCadastro.setSaldo(Double.parseDouble(edtQtd.getText().toString()));
+                materialController.addMaterial(materialCadastro, v);
             }
         });
+
+    }
+    public static void esconderTeclado(Context context,View view){
+        InputMethodManager inn=(InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        inn.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        //verificar comit 80
     }
 }
